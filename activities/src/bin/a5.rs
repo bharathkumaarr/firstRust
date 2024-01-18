6a294b7a0879d5513e345231a90c9a2b3becad29
Bharath Kumar Reddy <bharathkumarreddy@Bharaths-MacBook-Air.local>
2024-01-18 15:18:29 +0530

    a5

--- activities/src/bin/a5.rs
+++ activities/src/bin/a5.rs
@@ -9,4 +9,24 @@
 // * Print the variable within the loop statement
 // * Use break to exit the loop
 
-fn main() {}
+fn main() {
+
+  // mutable integer variable
+  let mut a = 1;
+
+  // loop statement
+  loop {
+
+    // Print the variable within the loop statement
+    println!("{:?}", a);
+
+    // break to exit the loop
+    if a == 4 {
+      break
+    }
+    
+    a = a + 1;
+    
+    
+  }
+}
