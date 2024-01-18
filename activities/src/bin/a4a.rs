a9b988f94ade3b755ada3f48a15fc0fe2ec5a5ad
Bharath Kumar Reddy <bharathkumarreddy@Bharaths-MacBook-Air.local>
2024-01-18 12:04:31 +0530

    a4a

--- activities/src/bin/a4a.rs
+++ activities/src/bin/a4a.rs
@@ -7,4 +7,13 @@
 // * Use a variable set to either true or false
 // * Use a match expression to determine which message to display
 
-fn main() {}
+fn main() {
+  // variable set to either true or false
+  let my_bool = false;
+
+  // match expression to determine which message to display
+  match my_bool {
+    true => println!("it is true"),
+    false => println!("it is false"),
+  }
+}
