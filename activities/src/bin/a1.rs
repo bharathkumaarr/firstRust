14acaab27bccc8de3dbb7eafb3ec0885de7b6a7c
Bharath Kumar Reddy <bharathkumarreddy@Bharaths-MacBook-Air.local>
2024-01-18 09:21:50 +0530

    Initial commit

---
+++ activities/src/bin/a1.rs
@@ -0,0 +1,24 @@
+// Topic: Functions
+//
+// Program requirements:
+// * Displays your first and last name
+//
+// Notes:
+// * Use a function to display your first name
+// * Use a function to display your last name
+// * Use the println macro to display messages to the terminal
+
+// function to display your first name
+fn first_name() {
+  println!("bharath kumar");
+}
+
+// function to display your last name
+fn last_name(){
+  println!("Reddy");
+}
+
+fn main() {
+first_name();
+last_name();
+}
